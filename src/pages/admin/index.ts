a29c9fbb65b556cbb04b865600d7773edb4175cf
Kehila <77875506s@gmail.com>
2025-11-25 08:52:55 -0400

    feat: Carrito-Perfil-Autores

--- src/pages/admin/index.ts
+++ src/pages/admin/index.ts
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 export { default as AuthorsAdminPage } from './AuthorsAdminPage';
-=======
->>>>>>> 5dd0a12edb31ae2c991160b380cc82b2985e5ef9
 export { default as CategoriesAdminPage } from './CategoriesAdminPage';
